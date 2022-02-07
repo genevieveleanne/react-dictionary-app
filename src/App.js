a4877a53cc4770eebd4cba5d53fd5eb59d21a510
genevieveleanne <genevieveleanned25@gmail.com>
2022-02-07 15:43:15 -0800

    Added some content to App and created DictionarySearch component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import DictionarySearch from "./DictionarySearch";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Hello World!</h1>
+        <h1>Dictionary</h1>
+        <DictionarySearch />
+        <footer>
+          <small>
+            This app was built by Genevieve Leanne Dominguez. It is open-sourced
+            on GitHub.
+          </small>
+        </footer>
       </div>
     </div>
   );
