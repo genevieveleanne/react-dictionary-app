6184d48195bdd28b1667146e4d31c6b84afc162f
genevieveleanne <genevieveleanned25@gmail.com>
2022-02-07 16:04:49 -0800

    Created basic search engine in DictionarySearch component

--- src/DictionarySearch.js
+++ src/DictionarySearch.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+
+import "./DictionarySearch.css";
 
 export default function DictionarySearch() {
-  return "Hello World";
+  let [word, setWord] = useState("");
+
+  function searchForWord(event) {
+    event.preventDefault();
+    alert(`Searching for definition of ${word}`);
+  }
+
+  function handleWordChange(event) {
+    setWord(event.target.value);
+  }
+
+  return (
+    <div className="DictionarySearch">
+      <form onSubmit={searchForWord}>
+        <input
+          type="search"
+          placeholder="Enter a Word"
+          onChange={handleWordChange}
+        />
+        <input type="submit" value="Find" />
+      </form>
+    </div>
+  );
 }
