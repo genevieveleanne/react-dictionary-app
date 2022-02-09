14b5170c23b3cb353803afb340a7e4e4a8b6ff77
genevieveleanne <genevieveleanned25@gmail.com>
2022-02-09 13:11:28 -0800

    Created Meanings component. Added part of speech, definitions, and examples.

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,12 +1,18 @@
 import React from "react";
+import Meanings from "./Meanings";
 
 export default function SearchResults(props) {
-  console.log(props.results);
-
   if (props.results) {
     return (
       <div className="SearchResults">
         <h2>{props.results.word}</h2>
+        {props.results.meanings.map(function (meaning, index) {
+          return (
+            <div key={index}>
+              <Meanings meaning={meaning} />
+            </div>
+          );
+        })}
       </div>
     );
   } else {
