8b9e73e73c9e7afb54c839dbd2b81beffdceb48f
genevieveleanne <genevieveleanned25@gmail.com>
2022-02-09 18:51:18 -0800

    Created Synonyms component. Added synonyms

--- src/Meanings.js
+++ src/Meanings.js
@@ -1,15 +1,21 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meanings(props) {
-  console.log(props.meaning);
   return (
     <div className="Meanings">
       <h4>{props.meaning.partOfSpeech}</h4>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>{definition.definition}</p>
-            <em>{definition.example}</em>
+            <p>
+              <strong>Definition:</strong> {definition.definition}
+              <br />
+              <strong>Sentence Example:</strong>
+              <em> {definition.example}</em>
+              <br />
+              <Synonyms synonyms={definition.synonyms} />
+            </p>
           </div>
         );
       })}
