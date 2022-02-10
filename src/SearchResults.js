8fe9d7c45d8a9c074a69c317e1b5afa26277491f
genevieveleanne <genevieveleanned25@gmail.com>
2022-02-09 19:25:12 -0800

    Created Phonetics component.

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,11 +1,16 @@
 import React from "react";
+import Phonetics from "./Phonetics";
 import Meanings from "./Meanings";
 
 export default function SearchResults(props) {
+  console.log(props.results);
   if (props.results) {
     return (
       <div className="SearchResults">
         <h2>{props.results.word}</h2>
+        <p>
+          <Phonetics phonetics={props.results.phonetics[0]} />
+        </p>
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
